84a15d21f26a29f4db93948f7f202746d483a317
unknown <vinicius130196@gmail.com>
2021-10-04 12:29:21 -0300

    Componente de login esta compilando

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { UserLogin } from '../Model/UserLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  emailLogin: UserLogin = new UserLogin()
 
-  ngOnInit(): void {
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0,0)
   }
-
+  entrar(){
+    this.auth.entrar(this.emailLogin).subscribe((resp:UserLogin)=>{
+    this.emailLogin=resp
+    
+    environment.token = this.emailLogin.token
+    environment.nome = this.emailLogin.nome
+    environment.foto = this.emailLogin.foto
+    environment.id = this.emailLogin.id
+  
+    console.log(environment.token)
+    console.log(environment.nome)
+    console.log(environment.foto)
+    console.log(environment.id)
+  
+    this.router.navigate(['/inicio'])
+    }, erro => {
+        if(erro.status ==500){
+          alert("Usuario ou senha estão incorretos!")
+        }
+      })
+    }
 }
